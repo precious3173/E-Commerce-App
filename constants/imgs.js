062d50f74eadc7142296fb9a73dc9df6dfc2df1c
Admin <preciouspeter3173@gmail.com>
2023-05-01 22:47:15 +0100

    Route created

---
+++ constants/imgs.js
@@ -0,0 +1,5 @@
+export default{
+bgPattern: require('../assets/anime.jpg'),
+user: require('../assets/anime-girl.jpg')
+
+};
---
+++ constants/routes.js
@@ -0,0 +1,21 @@
+export default{
+
+LOGIN: 'Login',
+REGISTER: 'Register',
+FORGOT_PASSWORD: 'Forgot Password',
+
+HOME: 'Home',
+HOME_TAB: 'Home Tab',
+HOME_DRAWER: 'Home Drawer',
+
+NOTIFICATIONS: 'Notifications',
+NOTIFICATIONS_DRAWER: 'Notification Drawer',
+
+SETTINGS: 'Settings',
+ACCOUNT_SETTINGS: 'Account Settings',
+SETTINGS_NAVIGATOR: 'Settings Navigator',
+
+SETTINGS_DETAIL: 'Settingd Detail',
+PROFILE: 'My Profile'
+
+};
