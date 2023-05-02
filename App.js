f66a03ba9729d20438fd1d960e2274b13c8e7326
Admin <preciouspeter3173@gmail.com>
2023-05-03 00:57:44 +0100

    app navigation added

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
 import React from 'react';
-import {Image, View} from "react-native"
-
-
-
-//alt + arrow
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-
-  
-return (
-    <View flex = {1} style ={ {backgroundColor: 'red'}}>
-<Image
-flex={1}
-source = {require ('./assets/anime.jpg')}
-/>
-
-<View
-w ="full"
-h = "full"
-position = "absolute"
-top = "0"
-px ="6"
-justifyContent = "center">
-
-<Heading>
-    SIGN IN
-</Heading>
-</View>
-</View>
- 
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
   );
 }
 
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import ROUTES from './routes';
+import IMGS from './imgs';
+import COLORS from '/colors';
+
+export{ROUTES, IMGS, COLORS};
+
