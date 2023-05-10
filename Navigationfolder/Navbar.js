fbd3cb47450ba87741a0edd024b5476aa72ea763
Admin <preciouspeter3173@gmail.com>
2023-05-10 23:27:38 +0100

    Navigation bar working

--- Navigationfolder/Navbar.js
+++ Navigationfolder/Navbar.js
@@ -1,25 +1,62 @@
 import * as React from 'react'
 import { SafeAreaView } from 'react-native';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import HomeScreen from './Screens/HomeScreen';
+import Itemscreen from './Screens/Itemscreen';
+import ProfileScreen from './Screens/ProfileScreen';
+
+
+// Screen names
+const homeName = 'Home';
+const profileName = 'Profile';
+const itemName = 'Items'
+
+
+const Tab = createBottomTabNavigator();
 
 function Navbar(){
 
 return(
 
-    <SafeAreaView>
-          <View styles ={
-            {flex:1, alignItems: 'center', justifyContent: 'center'}
-        }>
-         
-      <Text
-      onPress= {
-        ()=> navigation.navigate('Home')
-    }
-      >
-        Hello World
-      </Text>
-        </View>
-    </SafeAreaView>
+    <NavigationContainer>
+
+        <Tab.Navigator
+        initialRouteName = {homeName}
+        screenOptions = {({route}) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        let rn = route.name;
+
+                        if(rn === homeName){
+                            iconName = focused? 'home': 'home-outline'
+                        }
+                        else if (rn === profileName){
+                         iconName = focused? 'list' : 'profile-outline'
+
+                        }else if (rn === itemName){
+                            iconName = focused? 'item' : 'item-outline'
+   
+                           }
+                           return <Ionicons name= {iconName} size ={size}
+                           color={color}
+                           />
+                    },
+                })}
+        >
+
+           <Tab.Screen name ={ homeName} component = {HomeScreen}/>
+           <Tab.Screen name ={ profileName} component = {ProfileScreen}/>
+           <Tab.Screen name ={ itemName} component = {Itemscreen}/>
+
+
+
+
+        </Tab.Navigator>
+    </NavigationContainer>
    
 );
 
